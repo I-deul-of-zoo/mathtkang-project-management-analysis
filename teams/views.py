5e8e2d336d5fbe523e7fad4fd5a9ffb4ba4e1775
sgaeng <ksge1124@gmail.com>
2023-11-22 03:23:15 +0900

    :sparkles:Feat : 팀원 초대 승락 API 기능 추가 #7
    Close #7

--- teams/views.py
+++ teams/views.py
@@ -32,3 +32,34 @@ class TeamCreation(APIView):
             status=status.HTTP_400_BAD_REQUEST
         )
 
+
+class TeamInvitation(APIView):
+    '''
+    팀원 초대 API
+    '''
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, team_id, *args, **kwargs):
+        team = Team.objects.get(id=team_id)
+        
+        # 팀장만 초대 가능
+        if request.user not in team.owner.all():
+            return Response(
+                {"detail": "Only team owner can send invitations."}, 
+                status=status.HTTP_403_FORBIDDEN
+            )
+
+        username = request.data.get('username')
+        user_to_invite = User.objects.get(username=username)
+
+        if user_to_invite in team.members.all():
+            return Response(
+                {"detail": "User is already a member of the team."}, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        team.members.add(user_to_invite)
+        return Response(
+            {"detail": "Invitation sent successfully."}, 
+            status=status.HTTP_200_OK
+        )
